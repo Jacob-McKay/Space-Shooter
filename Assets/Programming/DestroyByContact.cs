cdbd2ff76f255e39a87d1e2a6a93e417f4eae955
Jacob McKay <jacob_mckay@bradycorp.com>
2015-05-18 09:04:48 -0500

    Next time we make things go BOOM

---
+++ Assets/Programming/DestroyByContact.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	//upon collision, destroy the bolt (other) and then this android
+	void OnTriggerEnter(Collider other) {
+
+		//the tutorial wanted you to do:
+		// if (other.tag == "Boundary"){ return; } but I think that's ugly
+
+		if (other.tag != "Boundary") {
+			Destroy	 (other.gameObject);
+			Destroy (gameObject);
+		}
+	}
+	
+}
