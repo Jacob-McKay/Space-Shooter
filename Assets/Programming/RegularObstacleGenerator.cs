bbfb642fadda82acee3b89df7f72cf1360363eb4
Jacob McKay <Jacob McKay>
2016-03-18 13:01:02 -0500

    Almost gutted appropriately
     - We are going to want to split the hazard generation out of the
       GameController
     - But for the most part you just have to drag some prefabs in and it
       works!

---
+++ Assets/Programming/RegularObstacleGenerator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//TODO generate asteroids and expose the stuff that is currently in GameController
+public class RegularObstacleGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
